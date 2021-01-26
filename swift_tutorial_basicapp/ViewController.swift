8dc71b70a0d6e8473380f54ff286c499b69b8f53
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-27 00:46:44 +0900

    complete

--- swift_tutorial_basicapp/ViewController.swift
+++ swift_tutorial_basicapp/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var tapCountLabel: UILabel!
+
+    var count = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tap(_ sender: Any) {
+        count = count + 1
+        countLabel.text = String(count)
 
+        switch count {
+        case 5:
+            imageView.image = UIImage(named: "back2")
+            tapCountLabel.text="5です"
+            break
+        case 10:
+            imageView.image = UIImage(named: "back3")
+            tapCountLabel.text="10です"
+            break
+        default:
+            tapCountLabel.text="5でも10でもありません"
+        }
+    }
+    
 }
 
